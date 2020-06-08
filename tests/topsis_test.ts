e7589640df95701d0ab8b80f5e76c0310a925500
shandysiswandi <shandysiswandi@gmail.com>
2020-06-08 17:25:40 +0700

    add indonesian and english reference for testing

--- tests/topsis_test.ts
+++ tests/topsis_test.ts
@@ -25,3 +25,34 @@ import { assertEquals, Topsis } from "./mod.ts";
 /**
  * Topsis Algorithm Test
  */
+const alternatif = ["Teacher A", "Teacher B", "Teacher C"];
+const kriteria = [
+  [80, 75, 80, 90, 85, 70, 65, 75],
+
+  [90, 75, 85, 70, 80, 65, 80, 75],
+
+  [75, 80, 70, 75, 85, 65, 70, 85],
+];
+const bobot = [10, 10, 20, 10, 15, 10, 10, 15];
+const tes = new Topsis(alternatif, kriteria, bobot).result();
+
+// contoh soal: http://serbaserbi-publik.blogspot.com/2016/10/spk-pemilihan-guru-teladan-dengan.html
+Deno.test("Hasil Perhitungan Topsis", () => {
+  assertEquals(tes, ["Teacher B", "Teacher A", "Teacher C"]);
+});
+
+// english
+const alternatives = ["MarutiErtiga", "Swift", "Tata Indica", "Alto800"];
+const criterias = [
+  [6, 7, 8, 6],
+  [8, 7, 8, 7],
+  [7, 9, 9, 8],
+  [9, 6, 8, 9],
+];
+const weights = [0.1, 0.4, 0.3, 0.2];
+const test = new Topsis(alternatives, criterias, weights).result();
+
+// Problems example: http://ijergs.org/files/documents/A-NEW20.pdf
+Deno.test("Topsis Calculation Results", () => {
+  assertEquals(test, ["Tata Indica", "Alto800", "Swift", "MarutiErtiga"]);
+});
