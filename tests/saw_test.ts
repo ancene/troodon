b2dd06cc3e1f29a063cd12443d52ce1d6640d095
shandysiswandi <shandysiswandi@gmail.com>
2020-06-10 17:44:31 +0700

    add saw test

--- tests/saw_test.ts
+++ tests/saw_test.ts
@@ -20,5 +20,29 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 ***************************************************************************** */
 
-export { assertEquals } from "https://deno.land/std/testing/asserts.ts";
-export { Topsis, AttributeSaw, SAW } from "../algorithms/mod.ts";
+import { assertEquals, SAW, AttributeSaw } from "./mod.ts";
+
+/**
+ * Simple Additive Weighting Algorithm Test
+ */
+const alternatives = ["A1", "A2", "A3", "A4", "A5"];
+const criterias = [
+  [80, 20, 20, 20, 20],
+  [40, 40, 40, 40, 40],
+  [60, 60, 60, 60, 100],
+  [80, 80, 80, 80, 80],
+  [40, 20, 40, 60, 100],
+];
+const weights = [20, 20, 15, 10, 30];
+const attributes = [
+  AttributeSaw.COST,
+  AttributeSaw.BENEFIT,
+  AttributeSaw.BENEFIT,
+  AttributeSaw.COST,
+  AttributeSaw.BENEFIT,
+];
+const test = new SAW(alternatives, criterias, weights, attributes);
+
+Deno.test("Hasil Perhitungan Topsis", () => {
+  assertEquals(test.result(), ["A3", "A4", "A5", "A2", "A1"]);
+});
