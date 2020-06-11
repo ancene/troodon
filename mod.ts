131f78359be6f339607d31c945cecde660f0e179
shandysiswandi <shandysiswandi@gmail.com>
2020-06-12 00:05:17 +0700

    separate modules

--- mod.ts
+++ mod.ts
@@ -2,4 +2,10 @@
 Copyright (c) the Trodoon authors. All rights reserved. MIT License.
 ***************************************************************************** */
 
-export { Topsis, SAW, AttributeSaw } from "./algorithms/mod.ts";
+import Topsis from "./algorithms/Topsis.ts";
+export { Topsis };
+
+import SAW, { Attribute as AttributeSaw } from "./algorithms/SAW.ts";
+export { SAW, AttributeSaw };
+
+export { isPrime } from "./algorithms/PrimeNumber.ts";
