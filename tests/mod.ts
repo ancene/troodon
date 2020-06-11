a142fa1f8a48bdcd7856725ac3e66088dcf9bd9d
shandysiswandi <shandysiswandi@gmail.com>
2020-06-11 22:41:17 +0700

    delete mod.ts in tests folder

--- tests/mod.ts
+++
@@ -1,6 +0,0 @@
-/*! *****************************************************************************
-Copyright (c) the Trodoon authors. All rights reserved. MIT License.
-***************************************************************************** */
-
-export { assertEquals } from "https://deno.land/std/testing/asserts.ts";
-export { Topsis, AttributeSaw, SAW } from "../algorithms/mod.ts";
