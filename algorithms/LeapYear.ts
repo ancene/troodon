6496d541979ff2859acd264da89d1e70378416fb
shandysiswandi <shandysiswandi@gmail.com>
2020-06-12 03:07:01 +0700

    add `LeapYear.ts` algorithm

---
+++ algorithms/LeapYear.ts
@@ -0,0 +1,32 @@
+/*! *****************************************************************************
+Copyright (c) the Trodoon authors. All rights reserved. MIT License.
+***************************************************************************** */
+
+import { IResult } from "./../interface.d.ts";
+
+const isLeapYear = (year: number): boolean =>
+  !(year % 4 || (!(year % 100) && year % 400));
+
+const listLeapYears = (s: number, e: number): IResult => {
+  let result: IResult;
+  const temp: Array<number> = [];
+
+  if (s < 1 || !Number.isInteger(s) || !Number.isInteger(e))
+    return (result = {
+      error: true,
+      length: 0,
+      result: [],
+    });
+  console.time("T1");
+  for (let i = s; i <= e; i++) {
+    if (isLeapYear(i)) temp.push(i);
+  }
+  console.timeEnd("T1");
+  return (result = {
+    error: false,
+    length: temp.length,
+    result: temp,
+  });
+};
+
+export { isLeapYear, listLeapYears };
