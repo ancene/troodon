13dbf74c1d73148dbf74ff3b53f09fbc0b3555f4
shandysiswandi <shandysiswandi@gmail.com>
2020-06-12 00:06:28 +0700

    delete `mod.ts` in algorithms folder

--- algorithms/mod.ts
+++
@@ -1,8 +0,0 @@
-/*! *****************************************************************************
-Copyright (c) the Trodoon authors. All rights reserved. MIT License.
-***************************************************************************** */
-
-import Topsis from "./Topsis.ts";
-import SAW, { Attribute as AttributeSaw } from "./SAW.ts";
-
-export { Topsis, SAW, AttributeSaw };
