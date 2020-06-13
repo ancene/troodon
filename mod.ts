d692b7a3bff6a9d802fc8895b7b51aa242385c66
shandysiswandi <shandysiswandi@gmail.com>
2020-06-13 13:36:29 +0700

    edit way to export

--- mod.ts
+++ mod.ts
@@ -2,14 +2,10 @@
 Copyright (c) the Trodoon authors. All rights reserved. MIT License.
 ***************************************************************************** */
 
-export { topsis } from "./algorithms/Topsis.ts";
-
-export { saw } from "./algorithms/SAW.ts";
-
-export { wpm } from "./algorithms/WPM.ts";
-
-export { Attribute, IResult } from "./interface.d.ts";
-
-export { isPrime, listPrimes } from "./algorithms/PrimeNumber.ts";
-
-export { isLeapYear, listLeapYears } from "./algorithms/LeapYear.ts";
+export * from "./algorithms/topsis.ts";
+export * from "./algorithms/saw.ts";
+export * from "./algorithms/wpm.ts";
+export * from "./algorithms/prime_number.ts";
+export * from "./algorithms/leap_year.ts";
+export * from "./enums.ts";
+export * from "./interfaces.ts";
